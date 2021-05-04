42a30a0612b62fc15eb753603d1553497edd162e
xTarzx <telmomartins2000@gmail.com>
2021-05-04 15:52:22 +0100

    allow user to setup custom domain for the url

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template, request, redirect
 from shortener import Shortener
 
+domain = input("If you have setup domain in HOSTS enter here >> ")
+if not domain or domain == "":
+    domain = "lazy.cnt"
+
+
 app = Flask("lazycnt")
 
 shortener = Shortener()
@@ -12,7 +17,7 @@ def main():
 @app.route("/short", methods=["POST"])
 def short():
     redirect_url = request.form["redirect_url"]
-    shortened_url = "http://lazy.cnt/{}".format(shortener.generate_key(redirect_url))
+    shortened_url = "http://{}/{}".format(domain, shortener.generate_key(redirect_url))
     return render_template("short.html", shortened_url=shortened_url)
 
 @app.route("/<key>")
